9aa230de9760cce45744fcefcc365f81aef24aa8
Vyrenil <varstaz@yahoo.com>
2022-01-29 12:04:03 +0100

    Added subtract function

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ import sys
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a-b
 
 main_a = 3
 main_b = 4
@@ -23,7 +25,7 @@ result_text = str(result_number)
 
 print('3 + 4 = ' + result_text)
 
+sub_result_num = sub(main_a, main_b)
+sub_result_text = str(sub_result_num)
 
-
-
-
+print('3 - 4 = ' + sub_result_text)
