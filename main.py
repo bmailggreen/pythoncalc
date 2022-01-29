7465fced38caae5fcec7596dc02a88f5c501680c
Gerjak Balazs <bmailggreen@gmail.com>
2022-01-29 11:25:29 +0100

    Initial version

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+class Ize:
+    def ize(self):
+        print("Fontos")
+        pass
+
+
+print("Helló Világ!!!!")
+ize = Ize()
+ize.ize()
+'''
+
+import sys
+
+def add(a, b):
+    return a + b
+
+
+main_a = 3
+main_b = 4
+result_number = add(main_a, main_b)
+result_text = str(result_number)
+
+print('3 + 4 = ' + result_text)
+
+
+
+
+
